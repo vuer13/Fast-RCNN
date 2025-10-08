e60c5437d26cbfb204a26eeebd96ea3eb7a1bdda
Ryan Vu <ryanvu1131@gmail.com>
2025-10-07 22:39:33 -0700

    set up before splitting

--- data_split.py
+++ data_split.py
@@ -8,3 +8,24 @@ from sklearn.model_selection import train_test_split
 dataset_root = 'data'
 all_images_dir = os.path.join(base_dir, "cards")
 coco_json_path = os.path.join(base_dir, "annotations.json")
+
+# Load json annotations
+with open(coco_json_path, "r") as f:
+    # coco now stores json f, which was opened
+    coco = json.load(f)
+    
+# Names of categories in json
+images = coco["images"]
+annotations = coco["annotations"]
+categories = coco["categories"]
+
+# Split images into 70%, 15%, 15%
+train_imgs, temp_imgs = train_test_split(images, train_size=0.7, random_state=42)
+val_imgs, test_imgs = train_test_split(temp_imgs, test_size=0.5, random_state=42)
+
+splits = {
+    "train": train_imgs,
+    "val": val_imgs,
+    "test": test_imgs
+}
+
