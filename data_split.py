3c3adfca1f86544f9593022d4edc0368853f4c96
Ryan Vu <ryanvu1131@gmail.com>
2025-10-09 11:09:19 -0700

    splitting data

--- data_split.py
+++ data_split.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 
 # Directories for cards
 dataset_root = 'data'
-all_images_dir = os.path.join(base_dir, "cards")
-coco_json_path = os.path.join(base_dir, "annotations.json")
+all_images_dir = os.path.join(dataset_root, "cards")
+coco_json_path = os.path.join(dataset_root, "annotations.json")
 
 # Load json annotations
 with open(coco_json_path, "r") as f:
@@ -29,3 +29,30 @@ splits = {
     "test": test_imgs
 }
 
+# Function to filter the annotations
+def filter_annotations(image_subset):
+    image_ids = {img["id"] for img in image_subset}
+    return [ann for ann in annotations if ann["image_id"] in image_ids]
+
+# Splitting images and annotations
+for split_name, subset_imgs in splits.items():
+    # Where all info goes per category
+    split_dir = os.path.join(dataset_root, split_name)
+    img_dir = os.path.join(split_dir, "images")
+    os.makedirs(img_dir, exist_ok=True)
+
+    subset_data = {
+        "images": subset_imgs,
+        "annotations": filter_annotations(subset_imgs),
+        "categories": categories
+    }
+
+    # Write new annotations.json
+    with open(os.path.join(split_dir, "annotations.json"), "w") as f:
+        json.dump(subset_data, f, indent=2)
+
+    # Copy images & duplicate
+    for img in subset_imgs:
+        src = os.path.join(all_images_dir, img["file_name"])
+        dst = os.path.join(img_dir, img["file_name"])
+        shutil.copy2(src, dst) 
