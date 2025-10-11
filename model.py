547b36188c6698142c9c8246d7d28718c52e20f3
Ryan Vu <ryanvu1131@gmail.com>
2025-10-10 23:13:29 -0700

    training code completed

--- model.py
+++ model.py
@@ -55,3 +55,60 @@ model.to(device)
 params = [p for p in model.parameters() if p.requires_grad]
 optimizer = torch.optim.SGD(params, lr=1e-3, momentum=0.9, weight_decay=0.0005) # to update weights in backward pass
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+# Training function - trains one epoch
+def train_one_epoch(model, optimizer, data_loader, device, epoch):
+    model.train()
+    for images, targets in data_loader:
+        # Move images to device
+        images = list(image.to(device) for image in images)\
+        
+        # Process targets - classnames and bounding box 
+        processed_targets = []
+        valid_images = []
+        for i, target in enumerate(targets):
+            boxes = []
+            labels = []
+            for obj in target:
+                # get bounding box 
+                bbox = obj['bbox']
+                x, y, w, h = bbox
+
+                if w > 0 and h > 0:
+                    boxes.append([x, y, x + w, y + h]) # x_min, y_min, x_max, y_max
+                    labels.append(obj['category_id'])
+            
+            # Only process is there are boxes in the image
+            if boxes:
+                processed_targets{
+                    'boxes': torch.tensor(boxes, dtype=torch.float32).to(device),
+                    'labels': torch.tensor(labels, dtype=torch.int64).to(device)
+                }
+                processed_targets.append(processed_targets)
+                valid_images.append(images[i]) # valid images only
+                
+        # skip if no valid targets
+        if not processed_targets:
+            continue
+        
+        images = valid_images
+        
+        # Forward pass - training the model
+        loss_dict = model(images, processed_targets)
+        losses = sum(loss for loss in loss_dict.values())
+        
+        # Backward pass
+        optimizer.zero_grad()
+        losses.backward()
+        optimizer.step()
+        
+    print(f"Epoch {epoch}, Loss: {losses.item()}")
+    
+epochs = 50
+for epoch in range(epochs):
+    train_one_epoch(model, optimizer, train_loader, device, epoch)
+    lr_scheduler.step()
+    
+    model_path = f'./model/fastrcnn_epoch_{epoch + 1}.pth'
+    torch.save(model.state_dict(), model_path)
+    print(f"Model saved to {model_path}")
